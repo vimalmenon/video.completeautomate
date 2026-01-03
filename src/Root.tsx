51e088eb4957c5d8faf1378a6142d4f85120a5df
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2026-01-03 11:03:51 +0800

    Add Intro component and update exports in Common directory

--- src/Root.tsx
+++ src/Root.tsx
@@ -27,21 +27,6 @@ export const RemotionRoot: React.FC = () => {
           logoColor2: "#86A8E7",
         }}
       />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-      <Composition
-        id="OnlyLogo"
-        component={Logo}
-        durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
-        schema={myCompSchema2}
-        defaultProps={{
-          logoColor1: "#91dAE2" as const,
-          logoColor2: "#86A8E7" as const,
-        }}
-      />
     </>
   );
 };
